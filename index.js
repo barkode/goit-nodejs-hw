37f010ed35975a9154250f57c9210f8a38128dd6
barkode <konstantinks@gmail.com>
2023-07-28 21:42:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-// index.js
-const argv = require("yargs").argv;
+console.log("I start dev");
+// // index.js
+// const argv = require("yargs").argv;
 
-// TODO: рефакторити
-function invokeAction({ action, id, name, email, phone }) {
-  switch (action) {
-    case "list":
-      // ...
-      break;
+// // TODO: рефакторити
+// function invokeAction({ action, id, name, email, phone }) {
+//   switch (action) {
+//     case "list":
+//       // ...
+//       break;
 
-    case "get":
-      // ... id
-      break;
+//     case "get":
+//       // ... id
+//       break;
 
-    case "add":
-      // ... name email phone
-      break;
+//     case "add":
+//       // ... name email phone
+//       break;
 
-    case "remove":
-      // ... id
-      break;
+//     case "remove":
+//       // ... id
+//       break;
 
-    default:
-      console.warn("\x1B[31m Unknown action type!");
-  }
-}
+//     default:
+//       console.warn("\x1B[31m Unknown action type!");
+//   }
+// }
 
-invokeAction(argv);
+// invokeAction(argv);
